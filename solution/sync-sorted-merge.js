8e4ab720e30eb0d7e92f8bd2aa968598e4d0f4e2
Andrea <andreamiralles686@gmail.com>
2017-09-08 16:27:45 -0700

    implement sync-sorted-merge

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,5 +1,22 @@
-'use strict'
+'use strict';
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
-}
+  
+  var merged = [];
+  for (var i = 0; i < logSources.length; i++) {
+    while (!logSources[i].drained) {
+      var test = logSources[i].pop();
+      // dont push if pop returns false
+      if (!!test) {
+        merged.push(test);
+      }
+    }
+  }
+  merged.sort((a, b) => {
+    return a.date - b.date;
+  });
+  
+  for (var i = 0; i < merged.length; i++) {
+    printer.print(merged[i]);
+  }
+};
