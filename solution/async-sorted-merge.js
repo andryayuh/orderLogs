910961d0611d0d453285f783e365433d015a5b50
Andrea <andreamiralles686@gmail.com>
2017-09-09 20:26:11 -0700

    add async solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,32 @@
-'use strict'
+const P = require('bluebird');
 
-module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
-}
+'use strict';
+
+var saveLogs = (sources, i, logStorage) => {
+  return sources[i].popAsync().then(val => {
+    if (val) {
+      logStorage.push(val);
+      return saveLogs(sources, i, logStorage);
+    } else if (sources[i + 1]) {
+      return saveLogs(sources, i + 1, logStorage);
+    } else {
+      return P.all(logStorage);
+    }
+  }).catch(err => { throw err; });
+};
+
+
+module.exports = (sources, printer) => {
+  let logStorage = [];
+  saveLogs(sources, 0, logStorage).then(logStorage => {
+
+    logStorage.sort((a, b) => {
+      return a.date - b.date;
+    });
+
+    for (var i = 0; i < logStorage.length; i++) {
+      printer.print(logStorage[i]);
+    }
+  }).catch(err => { throw err; });
+};
+
